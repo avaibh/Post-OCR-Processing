63845969bd97b5312b3fe0b2499c76d44f328fe6
Vaibhav Agrawal <vb8817@gmail.com>
2017-05-18 15:58:43 +0530

    Update OCR.cpp

--- OCR.cpp
+++ OCR.cpp
@@ -10,51 +10,84 @@
 #include "confusions.h"
 using namespace std;
 
+void loadNewConfusions(string& left_str, vector< pair <int, string> >& v_editDistance ,map<string,int>& lw_ConfPmap){
+    vector<string> lw_ConfP;
+    string str1, str2;
+    int edit_Distance;
+    str1 = left_str;
+
+    for( vector< pair <int, string> >::const_iterator eptr=v_editDistance.begin(); eptr!=v_editDistance.end(); eptr++)
+       {
+            edit_Distance = eptr->first;
+            str2 = eptr->second ;
+            appendConfusionsPairs((str1),(str2),lw_ConfP);//toslp1
+            //cout << str1 << " " << str2 << endl;
+           //vec.clear();
+       }
+       std::cout << "new confusions loop enetered " << '\n';
+
+    loadvectomap(lw_ConfP,lw_ConfPmap);
+}
+
 //function that takes dictionary and the left_word and return the top 10 edit distance as a vector
-vector< pair < int,string > > ocrDict_sorting(ifstream &dict, string &left_str)
-{ //function start
+void ocrDict_sorting(ifstream &dict, string &left_str, map<string,int>& lw_ConfPmap)
+{
     std::string line, word;
-    std::vector< pair <int, string> > v_editDistance;
+    vector< pair <int, string> > v_editDistance;
     while (getline(dict, line)){
         dict >> word;
         v_editDistance.push_back( make_pair ( editDistance(word,left_str), word ) );
     }
-    std::sort(v_editDistance.begin(), v_editDistance.end()); //sorting by edit distance
+    sort(v_editDistance.begin(), v_editDistance.end()); //sorting by edit distance
     while (v_editDistance.size() > 10) { // top 10 edit distances only
         v_editDistance.pop_back();
     }
     //std::cout << "I'm in vector function " << '\n';
-    return v_editDistance;
-} //function end
+    // loadNewConfusions( left_str,v_editDistance, map );
+}
 
 //Recursive function
-void ocrword_to_correctword(string &incorrect_word, ifstream &dict)
-{ //function start
-  std::string left_word, right_word, line, word;
-  std::vector< pair <int, string> > v_editDistance;
+string ocrword_to_correctword(string &incorrect_word, ifstream &dict)
+{
+  string left_word, right_word, line, word;
+  map<string,int> lw_ConfPmap;
+  int flag = 0;
 
-  for (int i = 0; i <= strlen(incorrect_word.c_str()); i++) {
-    left_word = incorrect_word.substr(0,i);
-    right_word = incorrect_word.substr(i+1, strlen(incorrect_word.c_str()));
-      while (getline(dict, line)) {
-        dict >> word;
-        if ( left_word.compare(word) == 0 ) {
-          //std::cout << "I'm in left_word == word if " << '\n';
-          ocrword_to_correctword(right_word, dict);
-          return;
+  while (getline(dict, line)){
+    dict >> word;
+    if (incorrect_word.compare(word) == 0) flag = 1; else flag =0;
+  }
+
+  if (flag == 1) { //if incorrect_word is in dictionary
+    return incorrect_word;
+  }else{
+      flag = 0;
+      for (size_t i = 0; i <= strlen(incorrect_word.c_str()); i++) {
+          left_word = incorrect_word.substr(0,i);
+          right_word = incorrect_word.substr(i, strlen(incorrect_word.c_str()));
+
+          while (getline(dict, line)) {
+            dict >> word;
+            if ( left_word.compare(word) == 0 ) {
+              //std::cout << "I'm in left_word == word if " << '\n';
+              flag = 1;
+            //  return a string to main
+            }else flag =0;
+          }
+          if (flag == 1) {
+            //ocrword_to_correctword(right_word, dict);
+          }else {
+          ocrDict_sorting(dict, left_word, lw_ConfPmap);
         }
       }
-       v_editDistance = ocrDict_sorting(dict, left_word);
-  }
-} //function end
+   }
+}
+
 
 //main function
 int main () {
-
-  string incorrect_ocrWord; //Incorrect word in SLP1 format
-  string line;
   map<string, int> dict_map, confusion_map, sandhi_map;
-  string word;
+  string word, line;
   int freq_word;
   size_t totalFreq_Dict = 0;
   //dictionary file
@@ -67,7 +100,6 @@ int main () {
       dict >> word;
       dict_map[word]++;
     }
-    dict.close();
   }
   else{
     std::cout << "file is not open" << '\n';
@@ -75,24 +107,23 @@ int main () {
 
   for( map<string,int>::const_iterator eptr=dict_map.begin(); eptr!=dict_map.end(); eptr++)
         {
-          totalFreq += (eptr->second) ;
+          totalFreq_Dict += (eptr->second) ;
         }
         //std::cout << totalFreq << '\n';
         //std::cout << 1/totalFreq << '\n';
-
-  //for confusions.h file. Load confusions from train pairs
-  map<string, int> ConfPmap1;
-  loadConfusions("/* filename */",ConfPmap1);
-  printmapWFreq(ConfPmap1);
-  return 0;
-
 /*
  *printing map of dictionary
  * for (std::map<string,int>::iterator it=dict_map.begin(); it!=dict_map.end(); ++it)
  * std::cout << it->first << " => " << it->second << '\n';
  */
 
-  ocrword_to_correctword(incorrect_ocrWord, dict);
+ //for confusions.h file. Load confusions from train pairs
+ map<string, int> ConfPmap1;
+ //loadConfusions("DataForVaibhav/TrainPairs.txt",ConfPmap1);
+ //printmapWFreq(ConfPmap1);
+
+ string ocrWord, correctWord; //Incorrect word in SLP1 format
+ correctWord = ocrword_to_correctword(ocrWord, dict);
 /*
   //confusion file
   size_t totalFreq_confusion = 0;
