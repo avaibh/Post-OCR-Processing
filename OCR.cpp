044733f75c05eee1c8d38194197961abf99ffc3b
Vaibhav Agrawal <vb8817@gmail.com>
2017-05-18 10:37:13 +0530

    Update OCR.cpp

--- OCR.cpp
+++ OCR.cpp
@@ -8,30 +8,6 @@
 #include <map>
 using namespace std;
 
-//Edit Distance
-int editDistance(std::string word1, std::string word2)
-{ //function start
-    int i, j, l1, l2, m;
-    l1 = word1.length();
-    l2 = word2.length();
-    vector< vector<int> > t(l1 + 1, vector<int>(l2 + 1));
-
-    for (i = 0; i <= l1; i++)
-        t[i][0] = i;
-    for (i = 1; i <= l2; i++)
-        t[0][i] = i;
-
-    for (i = 1; i <= l1; i++)
-    {
-        for (j = 1; j <= l2; j++)
-        {
-            m = min(t[i-1][j], t[i][j-1]) + 1;
-            t[i][j] = min(m, t[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1));
-        }
-    }
-    return t[l1][l2];
-} //function end
-
 //function that takes dictionary and the left_word and return the top 10 edit distance as a vector
 std::vector< pair < int,string > > ocrDict_sorting(std::ifstream &dict, std::string &left_str)
 { //function start
